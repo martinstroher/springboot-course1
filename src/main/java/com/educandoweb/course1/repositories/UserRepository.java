7de787282ffd44925f859f17175277e7c16061af
Martin Stroher <martinstrev@gmail.com>
2022-07-13 14:33:03 -0300

    Service layer, component registration

--- src/main/java/com/educandoweb/course1/repositories/UserRepository.java
+++ src/main/java/com/educandoweb/course1/repositories/UserRepository.java
@@ -1,6 +1,7 @@
 package com.educandoweb.course1.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.educandoweb.course1.entities.User;
 
